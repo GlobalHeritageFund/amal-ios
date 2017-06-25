f2d136ca1b3466054f3759a8a63f8743d7829c9a
Soroush Khanlou <soroushkhanlou@gmail.com>
2017-06-25 15:10:59 -0400

    move CGGeometry to its own file

---
+++ Aml/CGGeometry.h
@@ -0,0 +1,11 @@
+//
+//  CGGeometry.h
+//  Amal
+//
+//  Created by Soroush Khanlou on 6/25/17.
+//  Copyright © 2017 Dustin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+CGRect CGRectTrim(CGRect rect, CGFloat amount, CGRectEdge edge);
