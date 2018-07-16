21e4826cf9da58867b1e92d7e88e8d2b6ab621be
kenny <pearapps@gmail.com>
2018-07-16 16:32:29 -0400

    Adding a custom view controller in order to get the links added

---
+++ Aml/CautionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CautionViewController.h
+//  Amal
+//
+//  Created by Kenneth Parker Ackerson on 7/16/18.
+//  Copyright © 2018 Global Heritage Fund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CautionViewController : UIViewController
+
+@end
