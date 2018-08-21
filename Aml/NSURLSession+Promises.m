bd86c73670e15d27bcbcb74f889d0e9663f7afab
kenny <pearapps@gmail.com>
2018-08-21 17:55:18 -0400

    removing code

--- Aml/NSURLSession+Promises.m
+++ Aml/NSURLSession+Promises.m
@@ -14,59 +14,6 @@
 
 @implementation NSURLSession (Promises)
 
-- (Promise <NSDictionary *> *)POSTImageTo:(NSURL *)URL image:(UIImage *)image metadata:(NSDictionary *)metadata {
-    
-    NSData *data = UIImageJPEGRepresentation(image, 1.0);
-
-    MultipartComponent *imagePart = [[MultipartComponent alloc] initWithData:data name:@"image" fileName:@"image_1.jpg" contentType:@"image/jpeg"];
-    
-    NSMutableArray *parts = [[NSMutableArray alloc] initWithObjects:imagePart, nil];
-    
-    for (NSString *key in [metadata allKeys]) {
-        
-        id value = metadata[key];
-        
-        if ([value isKindOfClass:[NSNumber class]]) {
-            value = [value stringValue];
-        }
-        
-        NSData *data = [value dataUsingEncoding:NSUTF8StringEncoding];
-        MultipartComponent *part = [[MultipartComponent alloc] initWithData:data name:key fileName:nil contentType:@"text/plain"];
-
-        [parts addObject:part];
-    }
-    
-    return [self POSTMultipartWithURL:URL multiparts:parts];
-}
-
-- (Promise <NSDictionary *> *)POSTMultipartWithURL:(NSURL *)URL multiparts:(NSArray <MultipartComponent *> *)parts {
-    MultipartFormData *multipartData = [[MultipartFormData alloc] initWithParts:parts boundary:@"AMALBoundary"];
-    NSData *data = [multipartData dataRepresentation];
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:URL];
-    request.HTTPBody = data;
-    request.HTTPMethod = @"POST";
-    
-    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-    [request setValue:@"multipart/form-data; charset=utf-8; boundary=\"AMALBoundary\"" forHTTPHeaderField:@"Content-Type"];
-    [request setValue:[NSString stringWithFormat:@"%ld", [request.HTTPBody length]] forHTTPHeaderField:@"Content-Length"];
-
-    return [self taskWithRequest:request];
-}
-
-- (Promise <NSDictionary *> *)POSTJSONTaskWith:(NSURL *)URL JSONBody:(NSDictionary *)body {
-    
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:URL];
-    
-    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:body options:0 error:nil];
-    request.HTTPMethod = @"POST";
-    
-    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [request setValue:[NSString stringWithFormat:@"%ld", [request.HTTPBody length]] forHTTPHeaderField:@"Content-Length"];
-    
-    return [self taskWithRequest:request];
-}
-
 - (Promise <NSDictionary *> *)taskWithRequest:(NSURLRequest *)request {
     return [[[Promise alloc] initWithWork:^(void (^ _Nonnull fulfill)(NSData * _Nonnull), void (^ _Nonnull reject)(NSError * _Nonnull)) {
         
