8914b91fe58bcb460785749d9a5c6d2af30e72d7
Soroush Khanlou <soroushkhanlou@gmail.com>
2020-08-27 18:38:48 -0400

    we’re not localizing errors

--- Aml/Model/Promise/NSURLSession+Promises.m
+++ Aml/Model/Promise/NSURLSession+Promises.m
@@ -23,13 +23,13 @@
             if (error) {
                 reject(error);
             } else if(!(URLResponse.statusCode >= 200 && URLResponse.statusCode < 300)) {
-                NSString *errorString = NSLocalizedString(@"The body was empty.", @"");
+                NSString *errorString = @"The body was empty.";
                 NSString *body = [[NSString alloc] initWithData:data encoding:kCFStringEncodingUTF8];
                 if (body != nil) {
-                    errorString = [NSString stringWithFormat:@"The error was %@.", body]; // localize
+                    errorString = [NSString stringWithFormat:@"The error was %@.", body];
                 }
                 NSDictionary *userInfo = @{
-                    NSLocalizedDescriptionKey: [NSString stringWithFormat:@"The operation failed with a status code of %zd. %@", URLResponse.statusCode, errorString], // localize
+                    NSLocalizedDescriptionKey: [NSString stringWithFormat:@"The operation failed with a status code of %zd. %@", URLResponse.statusCode, errorString],
                 };
                 reject([NSError errorWithDomain:@"NSURLSessionStatusCodeError" code:URLResponse.statusCode userInfo:userInfo]);
             } else if (data) {
